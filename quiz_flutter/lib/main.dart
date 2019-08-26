af7114c5d5948c2ae3ebaf6df750f8214e6cb199
hikobe8 <hikobe8@163.com>
2019-08-26 11:38:56 +0800

    Hello Rectangle

--- quiz_flutter/lib/main.dart
+++ quiz_flutter/lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
+class HelloRectangle extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello Flutter',
+             style: TextStyle(
+               fontSize: 40.0
+             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 void main() {
-  print('start');
-  runApp(
-    Container(
-      color: Colors.greenAccent,
-    ),
-  );
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: HelloRectangle()
+      )));
 }
